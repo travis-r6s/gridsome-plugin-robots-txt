21599c3be98d145d7237f52e2c08594e8a4f336b
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-14 15:13:27 +0000

    Add simple robots txt generator

---
+++ index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const url = require( 'url' )
+const fs = require('fs')
+const generateRobotsTxt = require('generate-robotstxt')
+
+module.exports = (api, options) => {
+  api.afterBuild(async ({ queue, config }) => {
+    if (!config.siteUrl && !options.host) {
+      throw new Error(`Robots Txt plugin is missing a required siteUrl or host config.`)
+    }
+
+    let { host = config.siteUrl, sitemap, policy, output } = options
+
+    if (!sitemap) sitemap = url.resolve(host, 'sitemap.xml')
+
+    const robotsTxt = await generateRobotsTxt({ policy, sitemap, host })
+    const filename = path.join(config.outputDir, options.output)
+
+    return fs.writeFileSync(filename, robotsTxt)
+  })
+}
+
+module.exports.defaultOptions = () => ({
+  host: '',
+  sitemap: '',
+  policy: [],
+  output: 'robots.txt'
+})
